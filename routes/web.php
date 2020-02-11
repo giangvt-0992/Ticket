aeae9a8443bc1a1ef693354b25a3f6223cacbc22
vu.tuan.giang <vu.tuan.giang@sun-asterisk.com>
2020-02-11 16:59:23 +0700

    crud ticket

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'TicketsController@create');
+Route::post('/contact', 'TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit','TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
